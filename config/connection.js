3e0e4f51d3d26c761d0f085692f19edf638a2aeb
cplank <cplankshepard@gmail.com>
2019-04-10 19:53:07 -0700

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require('mysql');
 
 // MySQL DB Connection Information
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "PassWord",
-    database: "burrito_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "PassWord",
+        database: "burrito_db"
+    })
+};
 // Initiate MySQL Connection.
 connection.connect(function (err) {
     if (err) {
